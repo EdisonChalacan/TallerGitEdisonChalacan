c949532a147d7b0a71b5e3472c34135fa8ac577a
Edison Chalacan <edisondariochalacanmartinez@gmail.com>
2024-09-09 02:18:17 -0500

    Modificacion de botones y traduccion

--- traductor.js
+++ traductor.js
@@ -2,15 +2,24 @@ function numeroALetras(num) {
     const unidades = ["", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"];
     const decenas = ["", "diez", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa"];
     const especiales = ["once", "doce", "trece", "catorce", "quince"];
+    const centenas = ["", "cien", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos"];
 
-    if (num < 10) {
+    if (num === 0) {
+        return "cero";
+    } else if (num < 10) {
         return unidades[num];
     } else if (num >= 11 && num <= 15) {
         return especiales[num - 11];
-    } else if (num % 10 === 0) {
-        return decenas[Math.floor(num / 10)];
-    } else {
-        return decenas[Math.floor(num / 10)] + " y " + unidades[num % 10];
+    } else if (num < 100) {
+        return decenas[Math.floor(num / 10)] + (num % 10 !== 0 ? " y " + unidades[num % 10] : "");
+    } else if (num < 1000) {
+        return centenas[Math.floor(num / 100)] + (num % 100 !== 0 ? " " + numeroALetras(num % 100) : "");
+    } else if (num < 1000000) {
+        if (Math.floor(num / 1000) === 1) {
+            return "mil" + (num % 1000 !== 0 ? " " + numeroALetras(num % 1000) : "");
+        } else {
+            return numeroALetras(Math.floor(num / 1000)) + " mil" + (num % 1000 !== 0 ? " " + numeroALetras(num % 1000) : "");
+        }
     }
 }
 
@@ -18,15 +27,24 @@ function numeroAIngles(num) {
     const unidades = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const decenas = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
     const especiales = ["eleven", "twelve", "thirteen", "fourteen", "fifteen"];
+    const centenas = ["", "one hundred", "two hundred", "three hundred", "four hundred", "five hundred", "six hundred", "seven hundred", "eight hundred", "nine hundred"];
 
-    if (num < 10) {
+    if (num === 0) {
+        return "zero";
+    } else if (num < 10) {
         return unidades[num];
     } else if (num >= 11 && num <= 15) {
         return especiales[num - 11];
-    } else if (num % 10 === 0) {
-        return decenas[Math.floor(num / 10)];
-    } else {
-        return decenas[Math.floor(num / 10)] + " " + unidades[num % 10];
+    } else if (num < 100) {
+        return decenas[Math.floor(num / 10)] + (num % 10 !== 0 ? " " + unidades[num % 10] : "");
+    } else if (num < 1000) {
+        return centenas[Math.floor(num / 100)] + (num % 100 !== 0 ? " " + numeroAIngles(num % 100) : "");
+    } else if (num < 1000000) {
+        if (Math.floor(num / 1000) === 1) {
+            return "one thousand" + (num % 1000 !== 0 ? " " + numeroAIngles(num % 1000) : "");
+        } else {
+            return numeroAIngles(Math.floor(num / 1000)) + " thousand" + (num % 1000 !== 0 ? " " + numeroAIngles(num % 1000) : "");
+        }
     }
 }
 
